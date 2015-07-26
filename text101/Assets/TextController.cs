293da6bd932486064f5b8ead934a70bac27e6c7f
bionictruck <foofyjay@gmail.com>
2015-07-26 08:49:16 -0500

    Updates from lecture 31

--- text101/Assets/TextController.cs
+++ text101/Assets/TextController.cs
@@ -21,7 +21,11 @@ public class TextController : MonoBehaviour {
 	void Update () {
 	// If the space key is down then print the text
 		if (Input.GetKeyDown(KeyCode.Space)) {
-			text.text = "Space key was pressed";
+			// Wrapped text using + to concatenate and \n to add a space for new line. 
+			// \n\n adds a double space
+			text.text = "You are in a prison cell and you want to escape. There are some dirty sheets " +
+						"on the bed, a mirror on the wall and a locked door to your cell.\n\n" +
+						"Press S to view Sheets, M to view Mirror, L to view Lock";
 		}
 	
 	}
